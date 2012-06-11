dd2e545d3de6393942505a77cab15faf704fd1dc
Ian Mooney <ian@yoomee.com>
2012-06-11 17:55:26 +0100

    Email developers@yoomee.com new enquiries.

--- app/mailers/ym_enquiries/enquiry_mailer.rb
+++ app/mailers/ym_enquiries/enquiry_mailer.rb
@@ -2,7 +2,8 @@ class YmEnquiries::EnquiryMailer < ActionMailer::Base
   
   def new_enquiry(enquiry)
     @enquiry = enquiry
-    mail(:to => enquiry.email_to, 
+    mail(:to => enquiry.email_to,
+         :bcc => ["developers@yoomee.com", "andy@yoomee.com"],
          :from => enquiry.email_from, 
          :subject => enquiry.email_subject,
          :template_path => "enquiry_mailer")
