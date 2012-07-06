b31e09208f3ec620c0cf7cc55b388abc1a78254e
Ian Mooney <ian@yoomee.com>
2012-07-06 10:46:58 +0100

    Allow specific email templates for each enquiry form

--- app/mailers/ym_enquiries/enquiry_mailer.rb
+++ app/mailers/ym_enquiries/enquiry_mailer.rb
@@ -4,9 +4,19 @@ class YmEnquiries::EnquiryMailer < ActionMailer::Base
     @enquiry = enquiry
     mail(:to => enquiry.email_to,
          :bcc => ["developers@yoomee.com", "andy@yoomee.com"],
-         :from => enquiry.email_from, 
+         :from => enquiry.email_from,
          :subject => enquiry.email_subject,
-         :template_path => "enquiry_mailer")
+         :template_path => "enquiry_mailer",
+         :template_name => enquiry_template_name(enquiry))
+  end
+  
+  private
+  def enquiry_template_name(enquiry)
+    if ApplicationController.view_paths.any? {|path| Dir["#{path}/enquiry_mailer/#{enquiry.form_name}*"].present?}
+      enquiry.form_name
+    else
+      "new_enquiry"
+    end
   end
   
 end
